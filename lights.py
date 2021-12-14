30573c4743b588af0a086df7fbffbd063b162846
David Squire <dr.squire@protonmail.com>
2021-12-14 01:22:08 +0000

    Updated

--- lights.py
+++ lights.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import gpiozero
 from time import sleep
 RELAY_PIN = 18
@@ -5,10 +7,11 @@ RELAY_PIN = 18
 # Triggered by the output pin going high: active_high=True
 # Initially off: initial_value=False
 
-relay = gpiozero.OutputDevice(RELAY_PIN, active_high=True, initial_value=False)
+relay = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
 
 relay.off() # switch off
-
+sleep(1)
 relay.on() # switch on
+sleep(2)
 
 print(relay.value) # see if on or off
--- relay.py
+++ relay.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 GPIO.setwarnings(False)
-RELAIS_1_GPIO = 21
+RELAIS_1_GPIO = 18
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
 time.sleep(5)
-GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+# GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
