163f355dd40b6e668ad5099df75e01b6d684354f
VBARobotics <dr.squire@protonmail.com>
2021-12-22 01:57:58 +0000

    Updated relay

--- relay_on.py
+++ relay_on.py
@@ -1,9 +1,6 @@
 import RPi.GPIO as GPIO
-import time
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 GPIO.setwarnings(False)
 RELAIS_1_GPIO = 23
 GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
 GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
-time.sleep(5)
-# GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
